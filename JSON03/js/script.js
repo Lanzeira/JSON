4743b8a7b35c53999b855c16caeb1275ab2de435
Suporte - Alex <101653857+Lanzeira@users.noreply.github.com>
2023-04-11 15:34:06 -0300

    Atualização do treinamento JSON

--- JSON03/js/script.js
+++ JSON03/js/script.js
@@ -22,3 +22,25 @@ console.log(usado.contact.phone[1]);
 
 //console.log(user.contact.email[0]);
 //console.log(JSON.stringify(user));
+
+users_update = function() {
+    var x = new XMLHttpRequest();
+        x.onreadystatechange = function(){
+            let output = "";
+            if(this.readyState == 4 && this.status == 200){
+                let response = x.responseText;
+                let user= JSON.parse(response); 
+                console.log(response);
+                for (var i =0; i < user.length; i++) 
+                    output += "<li>"+ user[i].username + "</li>"; }
+                document.getElementById("users"),
+                innerHTML = output;
+            }
+        
+        x.open("GET", "file:///C:/xampp/htdocs/Alan_treino/JSON/JSON/JSON03/index.html", true);
+        x.send();
+    
+    var bt = document.getElementById("bt_update");
+    bt.addEventListener("click",users_update);
+}
+
